e30d1b69cae93059eefd9702c7ca11d805bc8052
mahaoyuan <1440970573@qq.com>
2021-12-21 13:23:50 +0800

    delete: useless code

--- accdoctor-user/src/main/java/com/bhjx/accdoctor/user/feign/OrderFeignService.java
+++ accdoctor-user/src/main/java/com/bhjx/accdoctor/user/feign/OrderFeignService.java
@@ -2,6 +2,7 @@ package com.bhjx.accdoctor.user.feign;
 
 import com.bhjx.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("accdoctor-order")
@@ -9,3 +10,5 @@ public interface OrderFeignService {
     @RequestMapping("/order/order/user/list")
     public R UserOrders();
 }
+
+
